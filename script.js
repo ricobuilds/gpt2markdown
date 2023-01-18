05356f3857958ba8403d7ceabb77e0d8204a2863
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-01-18 09:47:20 +0000

    c: added more fields to manifest, adding relevent styles to expoButton

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 // capturing the nextjs block
 const rootEle = document.querySelector('div[id="__next"]');
+const contEle = document.querySelector('.chatgptcont')
 
 
 // functions
 function handleClick() {
-    
+    console.log('you pressed me!')
 }
 
-function updateInterface() {}
+let expoButton = document.createElement('button');
+expoButton.classList.add('text-sm', )
+contEle.append(authButton)
+authButton.innerHTML = 'GPTExpo';
+authButton.addEventListener('click', handleClick);
+
+
+function updateInterface() {
+    // is the button there?
+    if (document.querySelector(".web-gptexpo-button")) return
+
+    // is the textarea there?
+    textarea = document.querySelector('textarea')
+    if (!textarea) return
+
+    let txtAreaWrapper = textarea.parentNode
+
+
+}
 
 
 // onLoad
-window.onload = () => {
+// window.onload = () => {
 
-    updateInterface()
-    
-    new MutationObserver(() => {
-        try {
-            updateInterface()
-        } catch (err) {
-            console.info("GPTExpo err found: Could not update the UI\n", err.stack)
-        }
-    }).observe(rootEle, {childList: true})
-}
+//     updateInterface()
+
+//     new MutationObserver(() => {
+//         try {
+//             updateInterface()
+//         } catch (err) {
+//             console.info("GPTExpo err found: Could not update the UI\n", err.stack)
+//         }
+//     }).observe(rootEle, {childList: true})
+// }
