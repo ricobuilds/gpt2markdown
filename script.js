dc350a9e3c0324882c1324fdc246b361247e82d5
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-01-18 19:52:32 +0000

    c: removed old parse code

--- script.js
+++ script.js
@@ -14,20 +14,6 @@ function handleClick() {
         const o = document.createElement("a");
         o.download = (document.querySelector(".pr-14.bg-gray-800")?.innerText || "Conversation with ChatGPT") + ".md", o.href = URL.createObjectURL(new Blob([t])), o.style.display = "none", document.body.appendChild(o), o.click()
     }, 3000);
-    
-    // function h() {
-    //     let gptCanvas = document.querySelector('html').innerHTML
-    //     return gptCanvas.replace(/<p>/g, '\n\n').replace(/<\/p>/g, '').replace(/<b>/g, '**').replace(/<\/b>/g, '**').replace(/<i>/g, '_').replace(/<\/i>/g, '_').replace(/<code[^>]*>/g, (match) => {
-    //         const lm = match.match(/class="[^"]*language-([^"]*)"/);
-    //         return lm ? '\n```' + lm[1] + '\n' : '```';
-    //     }).replace(/<\/code[^>]*>/g, '```').replace(/<[^>]*>/g, '').replace(/Copy code/g, '').replace(/This content may violate our content policy. If you believe this to be in error, please submit your feedback — your input will aid our research in this area./g, '').trim();
-    // }(() => {
-    //     const e = document.querySelectorAll(".text-base");
-    //     let t = "";
-    //     for (const s of e) s.querySelector(".whitespace-pre-wrap") && (t += `**${s.querySelector('img')?'You':'ChatGPT'}**: ${h(s.querySelector(".whitespace-pre-wrap").innerHTML)}\n\n`);
-    //     const o = document.createElement("a");
-    //     o.download = o.download = (document.querySelector(".pr-14.bg-gray-800")?.innerText || "Conversation with ChatGPT") + ".md", o.href = URL.createObjectURL(new Blob([t])), o.style.display = "none", document.body.appendChild(o), o.click()
-    // })();
 }
 
 expoButton.classList.add('font-medium', 'ml-1', 'md:ml-0', 'mt-0', 'md:mt-3', 'flex', 'items-center', 'justify-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
