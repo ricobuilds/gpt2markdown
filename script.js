ea11c12682dfbcaacd82cbc17f9b2a87cd7fd0d8
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-01-18 12:03:03 +0000

    testing extension in dev mode - c: modded mt values

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function handleClick() {
     }, 1200);
 }
 
-expoButton.classList.add('font-medium', 'ml-1', 'lg:ml-0', 'mt-3', 'lg:mt-0', 'flex', 'items-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
-contEle.append(expoButton)
+expoButton.classList.add('font-medium', 'ml-1', 'lg:ml-0', 'mt-0', 'lg:mt-3', 'flex', 'items-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
+// contEle.append(expoButton)
 expoButton.innerHTML = `
 <span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-export" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -25,36 +25,35 @@ expoButton.innerHTML = `
 `;
 exportButton.addEventListener('click', handleClick);
 
-exportButton.classList.add('font-medium', 'ml-1', 'lg:ml-0', 'mt-3', 'lg:mt-0', 'flex', 'items-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
-contEle.append(exportButton)
+exportButton.classList.add('font-medium', 'ml-1', 'lg:ml-0', 'mt-3', 'lg:mt-3', 'flex', 'items-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
 exportButton.innerHTML = `
 <span>Export to Notion</span>
 `;
 
 
 // modal
-expoModal.classList.add('hidden')
-contEle.append(expoModal);
-expoModal.innerHTML = `
-  <div class="modal-content">
-    <div class="modal-header">
-      <h2>Export conversation</h2>
-      <span class="close-button">&times;</span>
-    </div>
-    <div class="modal-body">
-      <!-- Your modal content goes here --> 
-      <select class="dbSelector">
-        <option>Database1</option>
-        <option>Database2</option>
-        <option>Database3</option>
-        <option>Database4</option>
-        <option>Database5</option>
-        <option>Database6</option>
-      </select>
-      <button class="font-medium ml-1 lg:ml-0 mt-3 lg:mt-0 flex items-center gap-2 text-sm rounded-md py-2 px-3 btn-primary">Export to Notion</button>
-    </div>
-  </div>
-`;
+// expoModal.classList.add('hidden')
+// contEle.append(expoModal);
+// expoModal.innerHTML = `
+//   <div class="modal-content">
+//     <div class="modal-header">
+//       <h2>Export conversation</h2>
+//       <span class="close-button">&times;</span>
+//     </div>
+//     <div class="modal-body">
+//       <!-- Your modal content goes here --> 
+//       <select class="dbSelector">
+//         <option>Database1</option>
+//         <option>Database2</option>
+//         <option>Database3</option>
+//         <option>Database4</option>
+//         <option>Database5</option>
+//         <option>Database6</option>
+//       </select>
+//       <button class="font-medium ml-1 lg:ml-0 mt-3 lg:mt-0 flex items-center gap-2 text-sm rounded-md py-2 px-3 btn-primary">Export to Notion</button>
+//     </div>
+//   </div>
+// `;
 
 
 function updateInterface() {
@@ -66,22 +65,23 @@ function updateInterface() {
     if (!textarea) return
 
     let txtAreaWrapper = textarea.parentNode
+
+    txtAreaWrapper.parentNode.insertBefore(expoButton, txtAreaWrapper.nextSibling);
 }
 
 
-// onLoad
-// window.onload = () => {
+window.onload = () => {
 
-//     updateInterface()
+    updateInterface()
 
-//     new MutationObserver(() => {
-//         try {
-//             updateInterface()
-//         } catch (err) {
-//             console.info("GPTExpo err found: Could not update the UI\n", err.stack)
-//         }
-//     }).observe(rootEle, {childList: true})
-// }
+    new MutationObserver(() => {
+        try {
+            updateInterface()
+        } catch (err) {
+            console.info("GPTExpo err found: Could not update the UI\n", err.stack)
+        }
+    }).observe(rootEle, {childList: true})
+}
 
 // else
 
@@ -89,7 +89,7 @@ var bottom = document.querySelector("div[class*='absolute bottom-0']");
 let footer = document.createElement('div')
 
 let extension_version = chrome.runtime.getManifest().version;
-footer.innerHTML = "<a href='https://github.com/0xreeko/gptexpo' target='_blank' class='underline'>GTPExpo extension v." + extension_version + "</a>. If you like the extension, please consider following me <a href='https://twitter.com/intent/follow?screen_name=emergingtechguy' target='_blank' class='underline'>@EmergingTechGuy</a>.";
+footer.innerHTML = "<a href='https://github.com/0xreeko/gptexpo' target='_blank' class='underline text-white'>GTPExpo extension v." + extension_version + "</a>. If you like the extension, please consider following me <a href='https://twitter.com/intent/follow?screen_name=emergingtechguy' target='_blank' class='underline text-white'>@EmergingTechGuy</a> on Twitter.";
 
 let lastEle = bottom.lastElementChild;
 lastEle.appendChild(footer);
