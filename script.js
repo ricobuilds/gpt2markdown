497902421acba50ea2feae545dc90fe58f06446d
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-01-21 12:00:26 +0000

    c: added else block to handleStore

--- script.js
+++ script.js
@@ -88,5 +88,23 @@ function handleStore() {
         if (result.buttonAdded) {
             console.info('GPT2Markdown button added - not adding again')
         }
+        else {
+            let expoButton = document.createElement('button');
+
+            expoButton.classList.add('gpt2markdown-export', 'font-medium', 'ml-1', 'md:ml-0', 'mt-0', 'md:mt-3', 'flex', 'items-center', 'justify-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
+            expoButton.innerHTML = `<span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-export" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3"></path>
+                </svg></span>
+                <span>GPT 2 Markdown</span>
+                `;
+            inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
+            inputActionNode.appendChild(expoButton)
+            expoButton.addEventListener('click', handleClick);
+            chrome.storage.local.set({ 'buttonAdded': true }, () => {
+                console.log('Button added to UI - state stored for persisting UI across chat switching')
+            })
+        }
     })
 }
