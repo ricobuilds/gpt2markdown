745fd4ae860ce52dc885cca856c43c01a42a1385
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-01-22 14:31:11 +0000

    c: ele reappend <works> but only on interval basis

--- script.js
+++ script.js
@@ -1,90 +1,120 @@
-// retrieve the button element from local storage
-let storedButton = sessionStorage.getItem("gptToMDButton");
-
-// check if the button element is stored in local storage
-if (storedButton) {
-    // create a new button element from the stored string
-    let expoButton = document.createElement('button');
-    expoButton.innerHTML = `${storedButton}`;
-
-    // append the button element to the DOM
-    inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
-    inputActionNode.appendChild(expoButton);
-
-    // add click event listener to the button
-    expoButton.addEventListener('click', handleClick);
-}
-
-// retrieve the footer element from local storage
-let storedFooter = sessionStorage.getItem("gptToMDFooter");
-
-// check if the footer element is stored in local storage
-if (storedFooter) {
-    // create a new footer element from the stored string
-    let footer = document.createElement('div');
-    footer.innerHTML = `${storedFooter}`;
-
-    // append the footer element to the DOM
-    var bottom = document.querySelector("div[class*='absolute bottom-0']");
-    let lastEle = bottom.lastElementChild;
-    lastEle.appendChild(footer);
-}
-
-// observer to check when the chat conversation changes
+// start
 const rootEle = document.querySelector('div[id="__next"]');
+
+// button
+let expoButton = document.createElement('button');
+
+expoButton.classList.add('gpt2markdown-export', 'font-medium', 'ml-1', 'md:ml-0', 'mt-0', 'md:mt-3', 'flex', 'items-center', 'justify-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
+expoButton.innerHTML = `
+    <span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-export" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+    <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3"></path>
+    </svg></span>
+    <span>GPT 2 Markdown</span>
+    `;
+inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
+inputActionNode.appendChild(expoButton)
+console.log(inputActionNode)
+expoButton.addEventListener('click', handleClick);
+
+// store the button element in local storage
+localStorage.setItem("gptToMDButton", expoButton.outerHTML);
+
 window.onload = () => {
     new MutationObserver(() => {
-        updateUI();
+        handleStore();
     }).observe(rootEle, {
         childList: true,
         subtree: true
-    });
-};
-
-window.onhashchange = function () {
-    // check if the hash fragment contains the conversation identifier
-    if (location.hash.includes('chat/')) {
-        //re-append the elements stored in session storage
-        updateUI();
-    }
+    })
 }
 
+// footer
+var bottom = document.querySelector("div[class*='absolute bottom-0']");
+let footer = document.createElement('div')
 
-// function to update the UI
-function updateUI() {
-    // retrieve the button element from local storage
-    let storedButton = sessionStorage.getItem("gptToMDButton");
-    // check if the button element is stored in local storage
-    if (storedButton) {
-        // create a new button element from the stored string
-        expoButton.innerHTML = `${storedButton}`;
+let extension_version = chrome.runtime.getManifest().version;
+footer.innerHTML = `<a href='https://github.com/0xreeko/gpt2markdown' target='_blank' class='underline text-white'>GPT2Markdown extension v.${extension_version}</a>. If you like the extension, please consider following me <a href='https://twitter.com/intent/follow?screen_name=emergingtechguy' target='_blank' class='underline text-white'>@EmergingTechGuy</a> on Twitter.`;
 
-        // append the button element to the DOM
-        inputActionNode.appendChild(expoButton);
+let lastEle = bottom.lastElementChild;
+lastEle.appendChild(footer);
+localStorage.setItem("gptToMDFooter", footer.outerHTML)
+console.log(lastEle)
+console.log(footer)
 
-        // add click event listener to the button
-        expoButton.addEventListener('click', handleClick);
-    }
 
-    // retrieve the footer element from local storage
-    let storedFooter = sessionStorage.getItem("gptToMDFooter");
-    // check if the footer element is stored in local storage
-    if (storedFooter) {
-        // create a new footer element from the stored string
-        footer.innerHTML = `${storedFooter}`;
-
-        // append the footer element to the DOM
-        lastEle.appendChild(footer);
-    }
-}
-
+// functions
 function handleClick() {
-    // Show a message to the user that the text has been copied to the clipboard
-    alert(`[GPT2Markdown]: « ${(document.querySelector(".pr-14.bg-gray-800")?.innerText)} » successfully exported!`);
+    setTimeout(() => {
+        // Show a message to the user that the text has been copied to the clipboard
+        alert(`[GPT2Markdown]: « ${(document.querySelector(".pr-14.bg-gray-800")?.innerText)} » successfully exported!`);
 
-    const e = document.querySelectorAll(".text-base");
-    let t = "";
-    for (const s of e) s.querySelector(".whitespace-pre-wrap") && (t += t == "" ? "" : "--------\n", t += `**${s.querySelectorAll('img').length > 1 ? 'You' : 'ChatGPT'}**: ${(s.querySelector(".whitespace-pre-wrap").innerHTML)}\n\n`);
-    const o = document.createElement("a");
-    o.download = (document.querySelector(".pr-14.bg-gray-800")?.innerText || "Conversation with ChatGPT") + ".md", o.href = URL.createObjectURL(new Blob([t])), o.style.display = "none", document.body.appendChild(o), o.click()
+        const e = document.querySelectorAll(".text-base");
+        let t = "";
+        for (const s of e) s.querySelector(".whitespace-pre-wrap") && (t += t == "" ? "" : "--------\n", t += `**${s.querySelectorAll('img').length > 1 ? 'You' : 'ChatGPT'}**: ${(s.querySelector(".whitespace-pre-wrap").innerHTML)}\n\n`);
+        const o = document.createElement("a");
+        o.download = (document.querySelector(".pr-14.bg-gray-800")?.innerText || "Conversation with ChatGPT") + ".md", o.href = URL.createObjectURL(new Blob([t])), o.style.display = "none", document.body.appendChild(o), o.click()
+    }, 500);
 }
+
+function updateUI() {
+    let storedButton = localStorage.getItem('gpt2MDButton')
+    let expoButton = document.createElement('button');
+    expoButton.innerHTML = storedButton
+    inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
+    inputActionNode.appendChild(localStorage.getItem(expoButton));
+
+    let storedFooter = localStorage.getItem('gptToMDFooter')
+    lastEle.appendChild(storedFooter)
+}
+
+function handleStore() {
+    textarea = document.querySelector('textarea')
+    if (!textarea) return
+
+    console.log(textarea)
+    existingButton = document.querySelector('.gpt2markdown-export')
+    if (!existingButton) {
+        console.log(existingButton)
+        expoButton.classList.add('gpt2markdown-export', 'font-medium', 'ml-1', 'md:ml-0', 'mt-0', 'md:mt-3', 'flex', 'items-center', 'justify-center', 'gap-2', 'text-sm', 'rounded-md', 'py-2', 'px-3', 'btn-primary')
+        expoButton.innerHTML = `<span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-export" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3"></path>
+                </svg></span>
+                <span>GPT 2 Markdown</span>
+                `;
+        inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
+        inputActionNode.appendChild(expoButton)
+        expoButton.addEventListener('click', handleClick);
+        expoButton.addEventListener('DOMNodeRemoved', function () {
+            chrome.storage.local.set({ 'isGTPToMDAdded': false });
+        });
+        chrome.storage.local.set({ 'isGTPToMDAdded': true })
+    }
+    existingFooter = document.querySelector("div[class*='absolute bottom-0']");
+    if (!existingFooter) {
+
+        let footer = document.createElement('div')
+
+        let extension_version = chrome.runtime.getManifest().version;
+        footer.innerHTML = `<a href='https://github.com/0xreeko/gpt2markdown' target='_blank' class='underline text-white'>GPT2Markdown extension v.${extension_version}</a>. If you like the extension, please consider following me <a href='https://twitter.com/intent/follow?screen_name=emergingtechguy' target='_blank' class='underline text-white'>@EmergingTechGuy</a> on Twitter.`;
+
+        let lastEle = bottom.lastElementChild;
+        lastEle.appendChild(footer);
+        localStorage.setItem("gptToMDFooter", footer.innerHTML)
+    }
+}
+
+targetNode = document.querySelector('a[class="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-gray-800 hover:bg-gray-800 group"]')?.innerText
+console.log(targetNode)
+
+// jhi
+setInterval(() => {
+    let innerText = document.querySelector('a[class="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-gray-800 hover:bg-gray-800 group"]')?.innerText;
+    if(innerText !== targetNode) {
+        previousInnerText = innerText;
+        handleStore();
+    }
+}, 2000);
