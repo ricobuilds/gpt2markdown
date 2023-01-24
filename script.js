3648ee13ebc3abca74f7e3081149e307a39ce7cd
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-01-24 21:27:45 +0000

    c: fix - opened up /images to avoid err on user DL

--- script.js
+++ script.js
@@ -16,8 +16,18 @@ expoButton.innerHTML = `
 inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
 inputActionNode.appendChild(expoButton)
 expoButton.addEventListener('click', handleClick);
+expoButton.addEventListener('load', () => console.log(document.querySelector(".pr-14.bg-gray-800")?.innerText))
 
-localStorage.setItem("gptToMDButton", expoButton.outerHTML);
+const handleLiveChat = () => {
+    setTimeout(() => {
+        chatText = document.querySelector(".pr-14.bg-gray-800")?.innerText
+        if (chatText) {
+            console.log('exist!')
+        } else {
+            console.log('not exist!')
+        }
+    }, 500);
+}
 
 new MutationObserver(() => {
     handleStore();
@@ -30,15 +40,15 @@ var bottom = document.querySelector("div[class*='absolute bottom-0']");
 let footer = document.createElement('div')
 
 let extension_version = chrome.runtime.getManifest().version;
-footer.innerHTML = `<a href='https://github.com/0xreeko/gpt2markdown' target='_blank' class='underline text-white'>GPT2Markdown extension v.${extension_version}</a>. If you like the extension, please consider following me <a href='https://twitter.com/intent/follow?screen_name=emergingtechguy' target='_blank' class='underline text-white'>@EmergingTechGuy</a> on Twitter.`;
+let extension_name = chrome.runtime.getManifest().name;
+footer.innerHTML = `<a href='https://github.com/0xreeko/gpt2markdown' target='_blank' class='underline text-white'>${extension_name} extension v.${extension_version}</a>. If you like the extension, please consider following me <a href='https://twitter.com/intent/follow?screen_name=emergingtechguy' target='_blank' class='underline text-white'>@EmergingTechGuy</a> on Twitter.`;
 
 let lastEle = bottom.lastElementChild;
 lastEle.appendChild(footer);
-localStorage.setItem("gptToMDFooter", footer.outerHTML)
-
 
 function handleClick() {
     if (document.querySelector(".pr-14.bg-gray-800")?.innerText === undefined) return
+    handleLiveChat()
     alert(`[GPT2Markdown]: « ${(document.querySelector(".pr-14.bg-gray-800")?.innerText)} » successfully exported!`)
 
     const e = document.querySelectorAll(".text-base");
@@ -75,7 +85,7 @@ function handleStore() {
 
         let lastEle = bottom.lastElementChild;
         lastEle.appendChild(footer);
-        localStorage.setItem("gptToMDFooter", footer.outerHTML)
+        // localStorage.setItem("gptToMDFooter", footer.outerHTML)
     }
 }
 // end
