e1563a67c5a15fa53ffd29759c672cdfe332b03b
Juan Valencia <juan7732@gmail.com>
2023-02-19 04:44:05 -0600

    Removing text change for now to not pollute PR

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ expoButton.innerHTML = `
     <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
     <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3"></path>
     </svg></span>
-    <span>GPT 2 Markdown Save</span>
+    <span>GPT 2 Markdown</span>
     `;
 inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
 inputActionNode.appendChild(expoButton)
@@ -85,7 +85,7 @@ function handleStore() {
             <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
             <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3"></path>
             </svg></span>
-            <span>GPT 2 Markdown Save</span>
+            <span>GPT 2 Markdown</span>
     `;
         inputActionNode = document.querySelector("div[class*='relative flex h-full flex-1 md:flex-col']");
         inputActionNode.appendChild(expoButton)
